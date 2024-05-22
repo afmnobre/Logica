ca04bb7281f9d1a8657490ca099875e53c9b4b1e
afmnobre <lucas_guitar1987@hotmail.com>
2024-05-21 23:22:51 -0300

    Tenho que modificar para o PHP.

---
+++ Busca_Boleana.php
@@ -0,0 +1,24 @@
+<?php
+
+function pesquisa_binaria($lista, $item){
+    $baixo = 0 
+    $alto = count($lista) - 1; 
+
+        while $baixo <= $alto{ 
+            $meio = ($baixo + $alto) / 2 
+            $chute = $lista[$meio]
+        if $chute == $item: 
+            return $meio
+            if $chute > $item: $alto = $meio - 1
+            else: 
+            $baixo = $meio + 1
+            return none 
+        }
+    }
+
+minha_lista = [1, 3, 5, 7, 9] 
+
+print pesquisa_binaria($minha_lista, 3) # => 1 ❾
+print pesquisa_binaria($minha_lista, -1) # => None
+
+?>
