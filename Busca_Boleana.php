dc3879427391f9bff6f8ae40c0223cbaa89d2376
afmnobre <lucas_guitar1987@hotmail.com>
2024-05-31 15:43:39 -0300

    Update

--- Busca_Boleana.php
+++ Busca_Boleana.php
@@ -16,9 +16,9 @@ function pesquisa_binaria($lista, $item){
         }
     }
 
-minha_lista = [1, 3, 5, 7, 9] 
+minha_lista = [1, 3, 5, 7, 9];
 
-print pesquisa_binaria($minha_lista, 3) # => 1 ❾
-print pesquisa_binaria($minha_lista, -1) # => None
+print pesquisa_binaria(minha_lista, 3); # => 1 ❾
+print pesquisa_binaria(minha_lista, -1); # => None
 
 ?>
